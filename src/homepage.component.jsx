1a6a00476d1f2ecd67fe7715eea5e05d4a2d35e0
Barbara Rekowska <basnow28@gmail.com>
2020-03-13 18:37:15 +0100

    Created project with create react app

---
+++ src/homepage.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HomePage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>HelloWorld</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default HomePage;
